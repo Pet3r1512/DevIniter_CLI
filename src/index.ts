160be6efca1f63ce8139586f464a8c0bd64d5800
Thanh Phong <pttp15122002@gmail.com>
2024-10-17 11:00:54 +0700

    Feat: CLI to install template based on user's choices

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,62 @@
 #!/usr/bin/env node
 
-import { input } from "@inquirer/prompts";
+import inquirer from "inquirer";
 import path from "path";
+import fs from "fs-extra";
+import { execaCommandSync } from "execa";
+import { fileURLToPath } from "url";
 
-const questions = [
-  {
-    type: "input",
-    name: "project name",
-    message: `What is your project's name: `,
-  },
-];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const templateDirectory = path.join(__dirname, "../templates");
 
-async function init() {
-  const answer = await input({ message: questions[0]?.message! });
+export async function init() {
+  try {
+    const answers = await inquirer.prompt([
+      {
+        type: "input",
+        name: "projectName",
+        message: `What is your project's name: `,
+      },
+      {
+        type: "list",
+        name: "template",
+        message: "Choose a template: ",
+        choices: ["Nextjs", "Vite"],
+      },
+    ]);
+
+    if (!answers) {
+      throw new Error("No answers received from inquirer prompt.");
+    }
+
+    const { projectName, template } = answers;
+    const projectPath = path.join(process.cwd(), projectName);
+    const templatePath = path.join(templateDirectory, template.toLowerCase());
+
+    console.log(`Project path: ${projectPath}`);
+    console.log(`Template path: ${templatePath}`);
+
+    fs.copySync(templatePath, projectPath);
+
+    // Read package.json from the template to get dependencies
+    const packageJsonPath = path.join(projectPath, "package.json");
+    const packageJson = fs.readJsonSync(packageJsonPath);
+    const dependencies = packageJson.dependencies || {};
+    const devDependencies = packageJson.devDependencies || {};
+
+    console.log("These packages will be installed:");
+    console.log("Dependencies:", Object.keys(dependencies).join(", "));
+    console.log("DevDependencies:", Object.keys(devDependencies).join(", "));
+
+    execaCommandSync("npm install", { cwd: projectPath, stdio: "inherit" });
+
+    console.log(
+      `Project ${projectName} created successfully using ${template} template 🚀.`
+    );
+  } catch (error) {
+    console.error("Error during initialization: ", error);
+  }
 }
 
 init();
