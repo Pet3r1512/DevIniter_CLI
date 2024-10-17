e30e1c30cba21cf6a752b73018f1e37bbac42b40
Thanh Phong <pttp15122002@gmail.com>
2024-10-17 07:45:25 +0700

    Feat: asking for project's name

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
-export const hello = () => {
-  return "Hello World";
-};
+
+import { input } from "@inquirer/prompts";
+import path from "path";
+
+const questions = [
+  {
+    type: "input",
+    name: "project name",
+    message: `What is your project's name: `,
+  },
+];
+
+async function init() {
+  const answer = await input({ message: questions[0]?.message! });
+}
+
+init();
