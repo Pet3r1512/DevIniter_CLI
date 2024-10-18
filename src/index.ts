ebb94b95b41b4107a0af9f0a9ae8224304aadd59
Thanh Phong <pttp15122002@gmail.com>
2024-10-18 10:00:09 +0700

    Refactor: extrac scaffoldTemplate process to a function for better testing

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,33 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const templateDirectory = path.join(__dirname, "../templates");
 
+export function scaffoldTemplate(projectName: string, template: string) {
+  const projectPath = path.join(process.cwd(), projectName);
+  const templatePath = path.join(templateDirectory, template);
+
+  console.log(`Project path: ${projectPath}`);
+  console.log(`Template path: ${templatePath}`);
+
+  fs.copySync(templatePath, projectPath);
+
+  const packageJsonPath = path.join(projectPath, "package.json");
+  const packageJson = fs.readJsonSync(packageJsonPath);
+  const dependencies = packageJson.dependencies || {};
+  const devDependencies = packageJson.devDependencies || {};
+
+  console.log("These packages will be installed:");
+  console.log("Dependencies:", Object.keys(dependencies).join(", "));
+  console.log("DevDependencies:", Object.keys(devDependencies).join(", "));
+
+  execaCommandSync("npm install", { cwd: projectPath, stdio: "inherit" });
+
+  console.log(
+    `Project ${projectName} created successfully using ${template} template 🚀.`
+  );
+
+  return true;
+}
+
 export async function init() {
   try {
     const answers = await inquirer.prompt([
@@ -30,29 +57,7 @@ export async function init() {
       throw new Error("No answers received from inquirer prompt.");
     }
 
-    const { projectName, template } = answers;
-    const projectPath = path.join(process.cwd(), projectName);
-    const templatePath = path.join(templateDirectory, template.toLowerCase());
-
-    console.log(`Project path: ${projectPath}`);
-    console.log(`Template path: ${templatePath}`);
-
-    fs.copySync(templatePath, projectPath);
-
-    const packageJsonPath = path.join(projectPath, "package.json");
-    const packageJson = fs.readJsonSync(packageJsonPath);
-    const dependencies = packageJson.dependencies || {};
-    const devDependencies = packageJson.devDependencies || {};
-
-    console.log("These packages will be installed:");
-    console.log("Dependencies:", Object.keys(dependencies).join(", "));
-    console.log("DevDependencies:", Object.keys(devDependencies).join(", "));
-
-    execaCommandSync("npm install", { cwd: projectPath, stdio: "inherit" });
-
-    console.log(
-      `Project ${projectName} created successfully using ${template} template 🚀.`
-    );
+    scaffoldTemplate(answers.projectName, answers.template.toLowerCase());
   } catch (error) {
     console.error("Error during initialization: ", error);
   }
