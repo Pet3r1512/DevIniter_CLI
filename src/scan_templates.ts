d6b6a25f8cefb2108ba458c388e7a1189ab9c282
Thanh Phong <pttp15122002@gmail.com>
2024-10-19 17:04:39 +0700

    Fix: duplicate variable

--- src/scan_templates.ts
+++ src/scan_templates.ts
@@ -1,17 +1,15 @@
 import fs from "node:fs";
 import path from "node:path";
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const templateDirectory = path.join(__dirname, "../templates");
 
 export function scanTemplates(): string[] {
-  const templates = fs
-    .readdirSync(path.join(__dirname, "../templates"))
-    .filter((file) => {
-      const filePath = path.join(templateDirectory, file);
-      return fs.statSync(filePath).isDirectory();
-    });
+  const templates = fs.readdirSync(templateDirectory).filter((file) => {
+    const filePath = path.join(templateDirectory, file);
+    return fs.statSync(filePath).isDirectory();
+  });
   return templates;
 }
