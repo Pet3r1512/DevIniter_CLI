ae72a3a7de043c3abbbdb8b6ffbe2f7d17ed810a
Thanh Phong <pttp15122002@gmail.com>
2024-10-19 15:43:09 +0700

    Feat: read templates automatically

--- src/scan_templates.ts
+++ src/scan_templates.ts
@@ -1,11 +1,13 @@
-import fs from "fs";
-import path from "path";
-import { templateDirectory } from "./index.js";
+import fs from "node:fs";
+import path from "node:path";
+const templateDirectory = path.join(__dirname, "../templates");
 
-export default function getTemplates(): string[] {
-  const templates = fs.readdirSync(templateDirectory).filter((file) => {
-    const filePath = path.join(templateDirectory, file);
-    return fs.statSync(filePath).isDirectory();
-  });
+export function scanTemplates(): string[] {
+  const templates = fs
+    .readdirSync(path.join(__dirname, "../templates"))
+    .filter((file) => {
+      const filePath = path.join(templateDirectory, file);
+      return fs.statSync(filePath).isDirectory();
+    });
   return templates;
 }
