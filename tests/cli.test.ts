fe94548c22dfdde10a1093f41468710a934ade54
Thanh Phong <pttp15122002@gmail.com>
2024-10-19 15:43:44 +0700

    Test: for cli

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -2,9 +2,9 @@ import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import path from "node:path";
 import { execaCommandSync } from "execa";
 import fs from "fs-extra";
-import { scaffoldTemplate } from "../dist/index.js";
+import { scaffoldTemplate } from "../src/index";
 
-const project_name = "test-project";
+const project_name = "testProject";
 const template = "Nextjs";
 const generatePath = path.join(__dirname, project_name);
 const projectPath = path.join(process.cwd(), project_name);
@@ -25,30 +25,12 @@ describe("CLI behaviors: ", () => {
   });
 
   it("should scaffold template and install dependencies", async () => {
-    const packageJson = {
-      dependencies: {
-        react: "^17.0.0",
-      },
-      devDependencies: {
-        typescript: "^4.0.0",
-      },
-    };
-
     const copySyncMock = vi.mocked(fs.copySync);
-    const readJsonSyncMock = vi
-      .mocked(fs.readJsonSync)
-      .mockReturnValue(packageJson);
     const execaCommandSyncMock = vi.mocked(execaCommandSync);
 
     const result = scaffoldTemplate(project_name, template);
 
-    expect(copySyncMock.mockImplementation(() => {})).toHaveBeenCalledWith(
-      templatePath,
-      projectPath
-    );
-    expect(readJsonSyncMock).toHaveBeenCalledWith(
-      path.join(projectPath, "package.json")
-    );
+    expect(copySyncMock).toHaveBeenCalledWith(templatePath, projectPath);
     expect(execaCommandSyncMock).toHaveBeenCalledWith("npm install", {
       cwd: projectPath,
       stdio: "inherit",
