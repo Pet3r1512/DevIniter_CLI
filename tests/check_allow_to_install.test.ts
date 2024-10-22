b39410fcd361b2a5cda06c6b107007bc2e1037db
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2024-10-23 06:50:05 +0700

    #TEST: Non-empty directory handler (#6)
    * Refactor: extract funciton to distinct file
    
    * Feat: import checkAllowToInstall function
    
    * Chore: update test command
    
    * Test: non-empty directory handler
    
    * Refactor: use camelCase instead
    
    * Refactor: test with VITEST_WORKER_ID
    
    * Refactor use "one-time mock resolved value" instead
    
    * Fix: misspelled
    
    * Refactor: remove unneccessary value
    
    * Fix: test's description

---
+++ tests/check_allow_to_install.test.ts
@@ -0,0 +1,73 @@
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import fs from "fs-extra";
+import path from "node:path";
+import { checkAllowToInstall } from "../src/check_allow_to_install";
+import inquirer from "inquirer";
+
+const TEST_TEMPLATE = "Nextjs";
+const promptSpy = vi.spyOn(inquirer, "prompt");
+
+describe("non-empty directory handler", () => {
+  let projectPath: string = "";
+
+  beforeEach(() => {
+    const workerId = process.env.VITEST_WORKER_ID;
+    const projectName = `testProject_${workerId}`;
+
+    projectPath = path.join(process.cwd(), projectName);
+
+    fs.ensureDirSync(projectPath);
+    fs.writeFileSync(path.join(projectPath, "somefile.txt"), "");
+  });
+
+  afterEach(() => {
+    fs.removeSync(projectPath);
+  });
+
+  it("should return true if the chosen directory is empty", async () => {
+    fs.emptyDirSync(projectPath);
+    const result = await checkAllowToInstall({
+      projectName: path.basename(projectPath),
+      template: TEST_TEMPLATE,
+    });
+    expect(result).toBe(true);
+  });
+
+  it("should return true and remove all files if user choose to remove all existing files", async () => {
+    promptSpy.mockResolvedValueOnce({ action: "remove" });
+
+    const result = await checkAllowToInstall({
+      projectName: path.basename(projectPath),
+      template: TEST_TEMPLATE,
+    });
+
+    expect(fs.readdirSync(projectPath).length).toBe(0);
+    expect(result).toBe(true);
+  });
+
+  it("should return false if user choose to cancel installation process", async () => {
+    promptSpy.mockResolvedValueOnce({
+      action: "cancel",
+    });
+
+    const result = await checkAllowToInstall({
+      projectName: path.basename(projectPath),
+      template: TEST_TEMPLATE,
+    });
+
+    expect(result).toBe(false);
+  });
+
+  it("should return true if the user chooses to ignore files and continue", async () => {
+    promptSpy.mockResolvedValueOnce({
+      action: "ignore",
+    });
+
+    const result = await checkAllowToInstall({
+      projectName: path.basename(projectPath),
+      template: TEST_TEMPLATE,
+    });
+
+    expect(result).toBe(true);
+  });
+});
