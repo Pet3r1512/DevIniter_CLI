257b3cee05211ffda506a032970e351834009b0f
Thanh Phong <pttp15122002@gmail.com>
2024-11-01 16:35:42 +0700

    Fix: imported component's file name

--- templates/vite/src/App.test.tsx
+++ templates/vite/src/App.test.tsx
@@ -1,10 +1,10 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import App from "./App";
+import App from "./app.tsx";
 
 describe("Index Page", () => {
-  it("should render Hi text", () => {
+  it("should render Starter Template text", () => {
     render(<App />);
     const starterText = screen.getByText("Starter Template");
     expect(starterText).toBeInTheDocument();
