43097ec5fd91affce51795e8acd8e8625694b28d
Thanh Phong <pttp15122002@gmail.com>
2024-11-02 08:51:07 +0700

    Chore: format document

--- templates/vite/src/components/ThemeToggle.tsx
+++ templates/vite/src/components/ThemeToggle.tsx
@@ -10,7 +10,7 @@ const ThemeToggle = ({ className }: { className?: string }) => {
       onClick={toggleTheme}
       className={cn(
         "h-8 lg:h-10 px-5 py-2 w-auto rounded-full lg:text-lg bg-gray-500 dark:bg-gray-200 transition-all dark:text-black text-white duration-150 ease-linear flex items-center justify-center",
-        className,
+        className
       )}
     >
       {theme === "light" ? "🌙 Dark" : "Light ☀️"}
