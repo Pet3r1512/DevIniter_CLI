98a7a92a12a74d3d21634050b0eca3743efd56ea
Thanh Phong <pttp15122002@gmail.com>
2024-11-05 08:28:42 +0700

    Style: add colors and config for shadcn's components

--- templates/vite/tailwind.config.js
+++ templates/vite/tailwind.config.js
@@ -3,12 +3,21 @@ export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: "#11998E",
-        secondary: "#38EF7D",
-      },
-    },
+  	extend: {
+  		colors: {
+  			primary: '#11998E',
+  			secondary: '#38EF7D',
+  			pink: {
+  				DEFAULT: '#ff99c8',
+  				dark: '#e88eed'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
