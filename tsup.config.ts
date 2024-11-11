7b7c6a0f6ec9591bd71ab1b6b151851302ece55d
Thanh Phong <pttp15122002@gmail.com>
2024-11-11 21:28:40 +0700

    Fix: remove dotenv

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "tsup";
-import dotenv from "dotenv";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  env: dotenv.config().parsed,
   format: ["esm", "cjs"],
   sourcemap: true,
   clean: true,
