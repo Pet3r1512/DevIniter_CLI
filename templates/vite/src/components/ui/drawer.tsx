1cf8da2a5a5ba85699fdfcb54d5e54a7391eb7f1
Thanh Phong <pttp15122002@gmail.com>
2024-11-15 14:46:37 +0700

    Chore: install husky and prettier for vite template

--- templates/vite/src/components/ui/drawer.tsx
+++ templates/vite/src/components/ui/drawer.tsx
@@ -42,7 +42,7 @@ const DrawerContent = React.forwardRef<
       ref={ref}
       className={cn(
         "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border border-neutral-200 bg-white dark:border-neutral-800 dark:bg-neutral-950",
-        className
+        className,
       )}
       {...props}
     >
@@ -82,7 +82,7 @@ const DrawerTitle = React.forwardRef<
     ref={ref}
     className={cn(
       "text-lg font-semibold leading-none tracking-tight",
-      className
+      className,
     )}
     {...props}
   />
