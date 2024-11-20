751e202b4b1ae19976b2c31a446b3e470f3c38b5
Thanh Phong <pttp15122002@gmail.com>
2024-11-20 10:12:23 +0700

    Feat: import scaffold template function

--- src/index.ts
+++ src/index.ts
@@ -3,48 +3,11 @@
 import inquirer from "inquirer";
 import path from "node:path";
 import { scanTemplates } from "./helpers/templatesScanner.js";
-import fs from "fs-extra";
-import ora from "ora";
 import { checkAllowToInstall } from "./helpers/checkAllowToInstall.js";
-import { installDependencies } from "./helpers/dependenciesInstaller.js";
+import { scaffoldTemplate } from "./helpers/scaffoldTemplate.js";
 
 const DEFAULT_TEMPLATES = ["nextjs", "vite"];
 export const templateDirectory = path.resolve(__dirname, "../templates");
-
-export async function scaffoldTemplate(projectName: string, template: string) {
-  const projectPath = path.join(process.cwd(), projectName);
-  const templatePath = path.join(templateDirectory, template);
-  const packageJsonPath = path.join(projectPath, "package.json");
-
-  if (!fs.existsSync(templatePath)) {
-    throw new Error(`Template ${template} does not exist.`);
-  }
-
-  const spinner = ora("Scaffolding template...").start();
-
-  try {
-    fs.copySync(templatePath, projectPath);
-
-    const packageJson = fs.readJsonSync(packageJsonPath);
-    packageJson.name = projectName;
-    fs.writeJsonSync(packageJsonPath, packageJson, { spaces: 2 });
-
-    spinner.succeed(
-      `Project ${projectName} created successfully using ${template} template 🚀.\n`
-    );
-
-    spinner.start();
-    await installDependencies(spinner, projectPath, projectName, {
-      silent: true,
-    });
-  } catch (error) {
-    spinner.fail(" Failed to install template.");
-    console.error((error as Error).message);
-    throw error;
-  }
-
-  return true;
-}
 
 export async function init() {
   try {
