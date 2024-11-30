d414740fb9ef8bf5f39df23bf200b409b6569ed1
Peter Pham <pttp15122002@gmail.com>
2024-11-30 16:06:14 +0700

    ops: detect yarn package manager

--- src/helpers/getPackageManager.ts
+++ src/helpers/getPackageManager.ts
@@ -1,10 +1,11 @@
-export type PackageManager = "npm" | "pnpm";
+export type PackageManager = "npm" | "pnpm" | "yarn";
 
 export const getPackageManager: () => PackageManager = () => {
   const userAgent = process.env.npm_config_user_agent;
 
   if (userAgent) {
     if (userAgent.startsWith("pnpm")) return "pnpm";
+    if (userAgent.startsWith("yarn")) return "yarn"
   }
   return "npm";
 };
