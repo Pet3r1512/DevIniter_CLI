c3dda10edec68a6b99c03204e93cb6088bfcaad9
Peter Pham <pttp15122002@gmail.com>
2025-02-12 11:24:19 +0700

    fix: bugs

--- templates/vitestack/vite.config.ts
+++ templates/vitestack/vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
-import path from "node:path";
+import { fileURLToPath } from "node:url"; // ✅ Use "node:" prefix
+import path from "node:path"; // ✅ Use "node:" prefix
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
