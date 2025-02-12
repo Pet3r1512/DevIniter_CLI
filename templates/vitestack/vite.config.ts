dfafbc75acdc15cf0950c104b50a96df7571fb22
Peter Pham <pttp15122002@gmail.com>
2025-02-12 09:27:29 +0700

    try

--- templates/vitestack/vite.config.ts
+++ templates/vitestack/vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import path from 'path';
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
