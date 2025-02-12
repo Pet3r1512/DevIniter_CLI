7641cfaf2727e6ce115f6906c379435006730905
Peter Pham <pttp15122002@gmail.com>
2025-02-12 11:27:52 +0700

    fix: use viteReact instead

--- templates/vitestack/vite.config.ts
+++ templates/vitestack/vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite";
-import { fileURLToPath } from "node:url"; // ✅ Use "node:" prefix
-import path from "node:path"; // ✅ Use "node:" prefix
-import react from "@vitejs/plugin-react";
+import path from "path";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import viteReact from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react()],
+  plugins: [TanStackRouterVite(), viteReact()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
